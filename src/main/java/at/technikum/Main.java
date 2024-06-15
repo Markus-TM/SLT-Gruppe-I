485548f4b6f1ba1bff20e339b5b15c4ebe7d6462
emreim <wi23b074@technikum-wien.at>
2024-06-15 14:03:06 +0200

    For Schleife hinzugefügt (#3)
    * For Schleife hinzugefügt
    
    * For Schleife angepasst

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -5,5 +5,9 @@ package at.technikum;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for(int i = 0; i < 4; i++) {
+            System.out.println("Test");
+        }
     }
 }
