934fe25c402b4cb66cc8386f48a91e30fc5bac7c
Emrehan Imamoglu <wi23b074@technikum-wien.at>
2024-06-15 13:57:30 +0200

    For Schleife angepasst

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        for(int i = 0; i < 5; i++) {
+        for(int i = 0; i < 4; i++) {
             System.out.println("Test");
         }
     }
